7c29238bbf6f0b8bfdb9d6d92ef3f30da7be1896
rosemaryW1 <rosemary.wakaba@starthmore.edu>
2023-03-07 16:00:58 +0300

    bootstrap

--- APIform.js
+++ APIform.js
@@ -1,6 +1,7 @@
 const form= document.querySelector('#searchForm');
 form.addEventListener('submit',async (e) => {
         e.preventDefault();
+        deleteImages();
         const searchTerm = form.elements.query.value;
         const res = await axios.get(` https://api.tvmaze.com/search/shows?q=${searchTerm}`);
         makeImages(res.data)
@@ -13,5 +14,12 @@ const makeImages=(shows)=>{
            img.src=result.show.image.medium;
            document.body.append(img);
         }
+    }
+}
+
+const deleteImages=()=>{
+    const images=document.querySelectorAll('img');
+    for(let image of images){
+        image.remove();
     }
 }
